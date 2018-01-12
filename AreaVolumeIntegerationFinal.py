aebae917b70670ca61f31f6c0835be04f24220e3
Sherif Hesham <sherif.hesham99@gmail.com>
2018-01-12 02:57:50 -0800

    Update AreaVolumeIntegerationFinal.py

--- AreaVolumeIntegerationFinal.py
+++ AreaVolumeIntegerationFinal.py
@@ -1,18 +1,19 @@
 #Submitted as a part of MTH111's Final group report @ EJUST - fall 2017
 #Python 3.6
-#Libraries: numpy,scipy, matplotlib
-
+#Libraries: numpy, scipy, matplotlib
 
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-try:
-    def getVolume():
-        ad= integrate.quad(fn, l, u)
-        print("Volume = " + str(round(np.pi*ad[0],3)))
 
+try:
+
+    def getVolume():
+        fnV= lambda x: (eval(fnin))**2
+        ad= integrate.quad(fnV, l, u)
+        print("Volume = " + str(round(np.pi*ad[0],3)))
         x = np.linspace(l, u, 600)
         eq= eval(fnin)
         revolve_steps = np.linspace(0, np.pi*2, 600).reshape(1,600)
@@ -27,9 +28,9 @@ try:
         ax.plot_surface(xs.T, y, z, color = 'blue', shade = True )
         plt.show()
 
-    def getArea():
-        
-        ad= integrate.quad(fn, l, u)
+    def getArea(fnin):
+        fnA= lambda x: (eval(fnin))
+        ad= integrate.quad(fnA, l, u)
         print("Area = " + str(round(ad[0],3)))
         
         x = np.linspace(l, u, num=1000)
@@ -39,7 +40,6 @@ try:
         plt.show()
 
     fnin= input("Function:")
-    fn= lambda x: (eval(fnin))**2
     l= int(input("From:"))
     u= int(input("To:"))
     req= input( "1)Area Under curve...2) Volume due to rotation around X axis)" )
